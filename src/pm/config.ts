66daa4bbb3189fd89736d538ab25944ea5f37b0a
mamun2519 <juborajislam46@gmail.com>
2025-03-24 10:48:15 +0600

    added to role hierarchy

--- src/pm/config.ts
+++ src/pm/config.ts
@@ -1,5 +1,6 @@
 export const RoleHierarchy: Record<string, string[]> = {
-      admin: ["manager"]
+  super_admin: ["admin"],
+  admin: ["manager"],
   manager: ["premium_user"],
   premium_user: ["user"],
   user: [],
