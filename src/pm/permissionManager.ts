f14a6491271fb26c67cc18442fd476a62ec6752a
mamun2519 <juborajislam46@gmail.com>
2025-03-24 11:09:23 +0600

    added to permission manager busniess logic

--- src/pm/permissionManager.ts
+++ src/pm/permissionManager.ts
@@ -2,3 +2,8 @@ interface PermissionContext {
   roles: string[];
   permissions: string[];
 }
+
+export class PermissionManager {
+  private readonly cachedRoleHierarchy: Map<string, Set<string>> = new Map();
+  private readonly cachedRolePermissions: Map<string, Set<string>> = new Map();
+}
