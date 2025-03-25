378b3ac078df2fbe12d908fe4f3315f9ecc26c8a
mamun2519 <juborajislam46@gmail.com>
2025-03-25 21:56:46 +0600

    added to permisssion manager

--- src/server.ts
+++ src/server.ts
@@ -1,4 +1,6 @@
 import { PermissionManager } from "./pm/permissionManager";
+import { RegistrationPolicy } from "./policy/policies/RegitrationPolicy";
+import { PolicyBuilder } from "./policy/policy";
 
 console.log("Hello world");
 
@@ -20,4 +22,8 @@ console.log(pm.hasAnyPermission(["product:read", "product:write"]));
 console.log("Max role is", pm.getMaxRole());
 console.log(pm.hasRole("super_admin"));
 
-const accessFreeTrail = (userid: number, email: string, password: string) => {};
+const accessFreeTrail = (userid: number, email: string, password: string) => {
+  const policyGroup = PolicyBuilder.create("FreeTrialPolicyGroup").addPolicy(
+    new RegistrationPolicy()
+  );
+};
