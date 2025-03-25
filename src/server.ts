fc751e23443fd8ec34fe416d0d25ae2544b028f6
mamun2519 <juborajislam46@gmail.com>
2025-03-25 19:30:54 +0600

    added to permisssion manager

--- src/server.ts
+++ src/server.ts
@@ -6,7 +6,7 @@ console.log("Hello world");
 const user = {
   id: 124,
   name: "Mohammad Mamun",
-  roles: ["admin", "user", "super_admin", "premium_user", "boss"],
+  roles: ["admin"],
   permissions: ["product:read"],
 };
 const pm = new PermissionManager({
@@ -18,3 +18,4 @@ console.log(pm.hasPermission("product:review"));
 console.log(pm.hesPermissions(["product:read", "product:delete"]));
 console.log(pm.hasAnyPermission(["product:read", "product:write"]));
 console.log("Max role is", pm.getMaxRole());
+console.log(pm.hasRole("super_admin"));
