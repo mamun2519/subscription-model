18860d7c9515b3f96413024f164ceb37b3a4de2f
mamun2519 <juborajislam46@gmail.com>
2025-03-25 21:59:21 +0600

    added to permisssion manager

--- src/server.ts
+++ src/server.ts
@@ -23,8 +23,33 @@ console.log(pm.hasAnyPermission(["product:read", "product:write"]));
 console.log("Max role is", pm.getMaxRole());
 console.log(pm.hasRole("super_admin"));
 
-const accessFreeTrail = (userid: number, email: string, password: string) => {
+const accessFreeTrial = async (
+  userid: number,
+  email: string,
+  password: string
+) => {
   const policyGroup = PolicyBuilder.create("FreeTrialPolicyGroup")
     .addPolicy(new RegistrationPolicy())
-    .addPolicy(new FreeTrialPolicy());
+    .addPolicy(new FreeTrialPolicy())
+    .build();
+
+  const { allowed, reason, name } = await policyGroup.can({
+    userId: userid,
+    email,
+  });
+  if (!allowed) {
+    console.error(
+      `[${name}] User ${userid} cannot access free trial: ${reason}`
+    );
+    return;
+  }
+  // do the main operation
+
+  console.log(`[${name}] User ${userid} can access free trial`);
+  return {
+    success: true,
+    message: `trial access granted for user ${userid}`,
+  };
 };
+
+accessFreeTrial(10, "hmnayem@stacklearner.com", "password");
