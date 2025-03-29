4c13c82ce6562fa955ce7c660b097322cbbe626c
mamun2519 <juborajislam46@gmail.com>
2025-03-29 21:21:05 +0600

    added to policy

--- src/policy/policies/PurchasePolicy.ts
+++ src/policy/policies/PurchasePolicy.ts
@@ -4,6 +4,4 @@ export class PurchasePolicy extends Policy {
   constructor() {
     super("PurchasePolicy", "Check if the user can purchase a product");
   }
-
-  async can(context: PolicyContext): Promise<PolicyResult> {}
 }
