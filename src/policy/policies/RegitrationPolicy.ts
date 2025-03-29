22543dc6bddca4d78b6106222dd395e44e6655de
mamun2519 <juborajislam46@gmail.com>
2025-03-29 21:22:46 +0600

    added to policy

--- src/policy/policies/RegitrationPolicy.ts
+++ src/policy/policies/RegitrationPolicy.ts
@@ -1,7 +1,1 @@
 import { Policy, PolicyContext, PolicyResult } from "../policy";
-
-export class RegistrationPolicy extends Policy {
-  constructor() {
-    super("RegistrationPolicy", "Check if the user can register");
-  }
-}
