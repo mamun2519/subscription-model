428e465ac4e55e45ba32f5e06352e829ed7088af
mamun2519 <juborajislam46@gmail.com>
2025-03-29 21:19:52 +0600

    added to policy

--- src/policy/policies/FreeTrialPolicy.ts.ts
+++ src/policy/policies/FreeTrialPolicy.ts.ts
@@ -1,7 +1,3 @@
 import { Policy, PolicyContext, PolicyResult } from "../policy";
 
-export class FreeTrialPolicy extends Policy {
-  constructor() {
-    super("FreeTrialPolicy", "Check if the user can access the free trial");
-  }
-}
+export class FreeTrialPolicy extends Policy {}
