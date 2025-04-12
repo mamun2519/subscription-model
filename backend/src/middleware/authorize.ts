c03b906480aacb8dfbb0ddbd239330ae78abe042
mamun2519 <juborajislam46@gmail.com>
2025-04-12 21:31:06 +0600

    added to middleware

--- backend/src/middleware/authorize.ts
+++ backend/src/middleware/authorize.ts
@@ -1,41 +1,42 @@
-import { NextFunction, Request, Response } from 'express';
+import { NextFunction, Request, Response } from "express";
 
 type AuthorizeOptions = {
-	permissions?: string | string[];
-	role?: string;
+  permissions?: string | string[];
+  role?: string;
 };
 
+//* middleware factory
 export const authorize = ({ permissions, role }: AuthorizeOptions) => {
-	return (req: Request, res: Response, next: NextFunction) => {
-		if (!req.pm) {
-			res.status(401).json({
-				message: 'Unauthorized',
-			});
-			return;
-		}
+  return (req: Request, res: Response, next: NextFunction) => {
+    if (!req.pm) {
+      res.status(401).json({
+        message: "Unauthorized",
+      });
+      return;
+    }
 
-		const checkRole = () => {
-			if (!role) return true;
-			return req.pm?.hasRole(role) ?? false;
-		};
+    const checkRole = () => {
+      if (!role) return true;
+      return req.pm?.hasRole(role) ?? false;
+    };
 
-		const checkPermissions = () => {
-			if (!permissions) return true;
-			if (Array.isArray(permissions)) {
-				return req.pm?.hasPermissions(permissions) ?? false;
-			}
-			return req.pm?.hasPermission(permissions) ?? false;
-		};
+    const checkPermissions = () => {
+      if (!permissions) return true;
+      if (Array.isArray(permissions)) {
+        return req.pm?.hasPermissions(permissions) ?? false;
+      }
+      return req.pm?.hasPermission(permissions) ?? false;
+    };
 
-		const hasAccess = checkRole() && checkPermissions();
+    const hasAccess = checkRole() && checkPermissions();
 
-		if (hasAccess) {
-			next();
-			return;
-		}
+    if (hasAccess) {
+      next();
+      return;
+    }
 
-		res.status(403).json({
-			message: 'Forbidden',
-		});
-	};
+    res.status(403).json({
+      message: "Forbidden",
+    });
+  };
 };
