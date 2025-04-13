be383f8645ac2680ce18892d5e7cea2295e18e90
mamun2519 <juborajislam46@gmail.com>
2025-04-13 22:26:17 +0600

    added to autehnticate  middleware

--- newsletter-backend/src/middleware/authenticate.ts
+++ newsletter-backend/src/middleware/authenticate.ts
@@ -7,4 +7,10 @@ export const authenticate = (
 ) => {
   //* receive token
   const token = req.headers.authorization?.split(" ")[1];
+  if (!token) {
+    res.status(401).json({
+      message: "Unauthorized",
+    });
+    return;
+  }
 };
