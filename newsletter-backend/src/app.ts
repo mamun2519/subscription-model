2f1b7935d8a53ee16e1571f20ff4b3ef519b8a01
mamun2519 <juborajislam46@gmail.com>
2025-04-13 22:12:01 +0600

    added to news letter backend

--- newsletter-backend/src/app.ts
+++ newsletter-backend/src/app.ts
@@ -3,10 +3,10 @@ dotenv.config();
 
 import cors from "cors";
 import express from "express";
-import { authenticate } from "./middleware/authenticate";
+// import { authenticate } from "./middleware/authenticate";
 import { User } from "./types";
 import { PermissionManager } from "./lib/pm/PermissionManager";
-import { authorize } from "./middleware/authorize";
+// import { authorize } from "./middleware/authorize";
 import { createProduct } from "./ProductService";
 
 //* define global namespace
@@ -24,36 +24,36 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-app.get(
-  "/",
-  authenticate,
-  authorize({ permissions: "product:read" }),
-  (req, res) => {
-    const pm = req.pm;
+// app.get(
+//   "/",
+//   authenticate,
+//   authorize({ permissions: "product:read" }),
+//   (req, res) => {
+//     const pm = req.pm;
 
-    try {
-      createProduct(
-        {
-          name: "Product 1",
-          price: 100,
-        },
-        {
-          userId: req.user?.sub!,
-          pm: req.pm!,
-        }
-      );
-    } catch {
-      res.status(403).json({
-        message: "You don't have permission to create products",
-      });
-    }
+//     try {
+//       createProduct(
+//         {
+//           name: "Product 1",
+//           price: 100,
+//         },
+//         {
+//           userId: req.user?.sub!,
+//           pm: req.pm!,
+//         }
+//       );
+//     } catch {
+//       res.status(403).json({
+//         message: "You don't have permission to create products",
+//       });
+//     }
 
-    res.json({
-      message: "Hello World",
-      user: req.user,
-      hasCreatePermission: pm?.hasPermission("product:manage"),
-    });
-  }
-);
+//     res.json({
+//       message: "Hello World",
+//       user: req.user,
+//       hasCreatePermission: pm?.hasPermission("product:manage"),
+//     });
+//   }
+// );
 
 export default app;
